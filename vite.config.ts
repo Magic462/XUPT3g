8a5fe9c75e52eab94274549e28b39ba9ad3d139f
SanYuan <2208939755@qq.com>
2025-03-23 23:03:25 +0800

    活动模块大致完成

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 这样 `@` 才能指向 `src`
+    },
+  },
 });
